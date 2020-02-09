ef7e143ea1a6178ec3988d58335db61bb89d98f4
Hannah Drury <hannah_drury@yahoo.com>
2020-02-09 19:15:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -92,7 +92,7 @@ def gross_per_studio(collection)
   # loop through collection to take the gross and add it to its corresponding studio name
   
   hash = {}
-  studioNames = [0]
+  studioNames = []
   
   i = 0
   # pp collection
